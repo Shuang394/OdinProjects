bbbe53c3ead3b2ebf03b73f402d0e3aff6499acc
Shuang <131367380+Shuang394@users.noreply.github.com>
2024-07-23 23:09:59 +0800

    Added function for game over checks

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -25,7 +25,7 @@ choice.forEach((item) => addEventListener('click', () => {
 }));
 
 //function to check if game is over
-function gameOver(board, index, player){
+function gameOver(board, index){
     switch (index){
         //check rows for center tiles, which have index 1 4 and 7
         case 1:
@@ -37,12 +37,45 @@ function gameOver(board, index, player){
             if ((board[2].player == board[4].player) && (board[4].player == board[6].player)){
                 return true;
             }
+        case 1:
         case 7:
-            //check for rows if center tiles are clicked on
+            //check for rows
             if ((board[index].player == board[index-1].player) && (board[index].player == board[index + 1].player)){
                 return true;
             }
 
+        //check rows for left side tiles, which have index 0, 3 and 6
+        case 0:
+            if ((board[0].player == board[4].player) && (board[4].player == board[8].player)){
+                return true;
+            }
+        case 3:
+        case 6:
+            if ((board[2].player == board[4].player) && (board[4].player == board[6].player)){
+                return true;
+            }
+
+            if ((board[index].player == board[index+1].player) && (board[index+1].player == board[index + 2].player)){
+                return true;
+            }
+
+        //check rows for right hand side tiles, which have index 2, 5 and 8
+        case 2:
+            if ((board[2].player == board[4].player) && (board[4].player == board[6].player)){
+                return true;
+            }
+        case 5:
+        case 8:
+            if ((board[0].player == board[4].player) && (board[4].player == board[8].player)){
+                return true;
+            }
+
+            if ((board[index].player == board[index-1].player) && (board[index-1].player == board[index -2].player)){
+                return true;
+            }
+
     }
+
+    return false;
 }
 
